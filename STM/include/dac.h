6527461f42c1d1c64a413e63f75eb11415dcd66a
ddefonce <duncanwdefonce@gmail.com>
2025-05-14 16:26:43 -0400

    Update dac.h

--- STM/include/dac.h
+++ STM/include/dac.h
@@ -1,8 +1,14 @@
+/*
+ * dac.h
+ * Wilson Wu, EE14, Spring 2025
+ *
+ */
+
 
 #include "ee14lib.h"
 #include <math.h> 
 
-#define MAX_DAC 4000 //actually 4k
+#define MAX_DAC 4095 //adjusts horn volume. maximum is 4095
 
 void dac_init (void);
 void trigger_horn (void);
